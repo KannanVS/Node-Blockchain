0e16cfc066649cef160885bb92e068b1724b4a6b
KannanVS <kannansanthanam97@gmail.com>
2020-08-13 01:01:19 +0200

    Blockchain creation

--- main.js
+++ main.js
@@ -13,3 +13,29 @@ class Block {
         return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
     }
 }
+
+class BlockChain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, Date.now(), 'Genesis', 0);
+    }
+
+    getLastestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLastestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let zeni = new BlockChain();
+zeni.addBlock(new Block(0, '13/08/2020', { amount : 10 }));
+zeni.addBlock(new Block(0, '14/08/2020', { amount : 20 }));
+
+console.log(JSON.stringify(zeni, null, 4));
